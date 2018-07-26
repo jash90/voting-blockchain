ac949bdaf919848dc41651f34f26608308f154e5
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2018-07-26 21:41:21 +0200

    dodanie komponentu Bar do wyświetlania wyników ankiety

---
+++ src/components/bar/index.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    Dimensions
+} from 'react-native'
+import PropTypes from 'prop-types';
+
+export default class Bar extends Component {
+    render() {
+        return (
+             <View style={{flexDirection:'column'}}>
+                    <View style={{ flexDirection: 'row' }}>
+                        <View style = {{ backgroundColor: this.props.background, borderColor: this.props.fill, borderWidth:4, width:Dimensions.get('window').width*0.55, height:this.props.height}} >
+                        </View>
+                        <View style={{ backgroundColor: this.props.fill, position: 'absolute', top: 0, left: 0, width: Dimensions.get('window').width * 0.55*this.props.procentage/100, height:this.props.height }}>
+                        </View>
+                        <View style={{justifyContent:'center',marginLeft:20, }}>
+                            <Text style={{fontWeight:'bold', fontSize:this.props.fontSize}}>
+                                {this.props.procentage+"%"}
+                            </Text>
+                        </View>
+                    </View>
+                </View>
+        );
+    }
+}
+
+Bar.propTypes = {
+  procentage: PropTypes.number.isRequired,
+};
+
+Bar.defaultProps = {
+  procentage: 56,
+  fill:'black',
+  background:'white',
+  fontSize:22,
+  height:50
+};
