3e304d3b4b2135bf2658577c41a73908a0a42b1d
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2018-09-16 16:25:37 +0200

    dodanie bottomnavigationbar

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import Login from './src/scene/Login';
 import Item from './src/scene/Item';
 import Item2 from './src/scene/Item2';
 import Register from './src/scene/Register';
+import Tabs from './src/scene/Tabs';
 import {MenuProvider} from "react-native-popup-menu"
 export default class App extends Component {
   render() {
@@ -17,6 +18,7 @@ export default class App extends Component {
             <Scene key="Item" component={Item} hideNavBar/>
             <Scene key="Item2" component={Item2} hideNavBar/>
             <Scene key="Register" component={Register} hideNavBar/>
+            <Scene key="Tabs" component={Tabs} hideNavBar initial/>
           </Stack>
         </Router>
       </MenuProvider>
