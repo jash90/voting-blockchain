dbe60961e81cea22a838e407c981590d91eb9fe2
Bartłomiej Zimny <bartlomiejzimny@outlook.com>
2018-11-25 00:45:57 +0100

    upgrade reacta i mobxa

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-import { AppRegistry } from 'react-native';
-import { name as appName } from "./app.json";
-import applyDecoratedDescriptor from "@babel/runtime/helpers/es6/applyDecoratedDescriptor";
-import initializerDefineProperty from "@babel/runtime/helpers/es6/initializerDefineProperty";
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 
-Object.assign(babelHelpers, {applyDecoratedDescriptor, initializerDefineProperty});
-
-AppRegistry.registerComponent(appName, () => require("./App").default);
+AppRegistry.registerComponent(appName, () => App);
